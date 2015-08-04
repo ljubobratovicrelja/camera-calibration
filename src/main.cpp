287606f7fb1a75d9fbdda6d142744bf9f12b7cf1
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-04 21:29:15 +0200

    added detection routines

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,29 @@
+// The MIT License (MIT)
+// 
+// Copyright (c) 2015 Relja Ljubobratovic, ljubobratovic.relja@gmail.com
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+// 
+// Author:
+// Relja Ljubobratovic, ljubobratovic.relja@gmail.com
+
+
 #include <io.hpp>
 #include <gui.hpp>
 #include <image.hpp>
@@ -7,35 +33,88 @@
 #include <draw.hpp>
 
 #include <iostream>
+#include <fstream>
 
 #include "calibpattern.hpp"
 
+bool write_pattern_results(const std::vector<std::vector<cv::vec2r> > &patterns, const std::string &path) {
+
+	std::ofstream stream(path, std::fstream::out);
+
+	if (!stream.is_open()) {
+		return false;
+	}
+
+	for (unsigned i = 0; i < patterns.size(); ++i) {
+		stream << std::to_string(patterns[i][0][0]) << " " << std::to_string(patterns[i][0][1]);
+		for (unsigned j = 1; j < patterns[i].size(); ++j) {
+			stream << "," << std::to_string(patterns[i][j][0]) << " " << std::to_string(patterns[i][j][1]);
+		}
+		stream << std::endl;
+	}
+
+	stream.close();
+
+	return true;
+}
+
 void pattern_detection() {
 
-	auto image = cv::imread("/home/relja/calibration/ground_truth/2.png", cv::REAL, 1);
+	std::string calib_patterns[] = {"1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"};
 
-	unsigned p_rows = 6;
-	unsigned p_cols = 9;
+	std::vector<std::vector<cv::vec2r> > patterns;
 
-	cv::matrixr im_r = image, im_rb;
-	cv::matrixr gauss_k = cv::gauss({3, 3});
+	for (auto im_path : calib_patterns) {
 
-	im_rb = cv::conv(im_r, gauss_k);
+		auto image = cv::imread("/home/relja/calibration/ground_truth/" + im_path, cv::REAL, 1);
 
-	auto pattern = detect_pattern(im_rb, p_rows, p_cols, 18., 0.15);
+		if (image.rows() > 512) {
+			cv::resize(
+					}
+		unsigned p_rows = 6;
+		unsigned p_cols = 9;
 
-	if (pattern.size() == p_rows*p_cols) {
+		cv::matrixr im_r = image, im_rb;
+		cv::matrixr gauss_k = cv::gauss({3, 3});
 
-		pattern = sub_pixel_detect(pattern, im_r, {5, 5});
+		im_rb = cv::conv(im_r, gauss_k);
 
-		cv::matrix3r im_draw = im_r.clone();
-		draw_chessboard(im_draw, pattern, {255., 0., 0.});
+		auto pattern = detect_pattern(im_rb, p_rows, p_cols, 15., 0.15, 10);
 
-		cv::imshow("pattern", im_draw);
-		cv::wait_key();
+		if (pattern.size() == p_rows*p_cols) {
+
+			pattern = sub_pixel_detect(pattern, im_r, {5, 5});
+
+			cv::matrix3r im_draw = im_r.clone();
+			draw_chessboard(im_draw, pattern, {255., 0., 0.});
+
+			cv::imshow("pattern", im_draw);
+			auto r = cv::wait_key();
+
+			if (r == 'n') {
+				std::cout << "Pattern " << im_path << " rejected." << std::endl;
+				continue;
+			} else if (r == 'q') {
+				std::cout << "Pattern detection exit" << std::endl;
+				break;
+			} else {
+				std::cout << "Pattern " << im_path << " accepted." << std::endl;
+				patterns.push_back(pattern);
+			}
+		} else {
+			std::cout << "Pattern " << im_path << " not found" << std::endl;
+			continue;
+		}
+	}
+
+	std::string out_path;
+	std::cout << "Where to write results?" << std::endl;
+	std::cin >> out_path;
+
+	if (write_pattern_results(patterns, out_path)) {
+		std::cout << "Writing results to " << out_path << " successfull!" << std::endl;
 	} else {
-		std::cout << "Pattern not found" << std::endl;
-		return;
+		std::cout << "Writing results to " << out_path << " failed!" << std::endl;
 	}
 }
 
