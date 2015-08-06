997a9654a9c273879f1be7e670ed6bb7cf499ad2
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-06 20:15:40 +0200

    added least squares homography calculation, fixed DLT

--- src/homography.cpp
+++ src/homography.cpp
@@ -54,7 +54,6 @@ cv::matrixr DLT_pointSimilarityEstimation(const std::vector<cv::vec2r> &features
 	transform(1, 1) = scale_v;
 	transform(0, 2) = centroid[0];
 	transform(1, 2) = centroid[1];
-	transform(2, 2) = centroid[2];
 
 	return transform;
 }
@@ -81,6 +80,7 @@ void DLT(const std::vector<cv::vec2r> &src_pts, const std::vector<cv::vec2r> &tg
 
 	// 1. Perform normalization;
 	srcS = DLT_pointSimilarityEstimation(src_pts);
+	std::cout << srcS << std::endl;
 	tgtS = DLT_pointSimilarityEstimation(tgt_pts);
 
 	auto src_n = src_pts; // source normalized points
@@ -93,27 +93,23 @@ void DLT(const std::vector<cv::vec2r> &src_pts, const std::vector<cv::vec2r> &tg
 	DLT_normalize(tgt_n, tgtS);
 
 	// 2. Pack matrix A;
-	#pragma omp parallel
-	{
-		#pragma omp for
-		for (unsigned i = 0; i < src_pts.size(); ++i) {
-			// [-x -y -1 0 0 0 ux uy u]
-			// [0 0 0 -x -y -1 vx vy v]
+	for (unsigned i = 0; i < src_pts.size(); ++i) {
+		// [-x -y -1 0 0 0 ux uy u]
+		// [0 0 0 -x -y -1 vx vy v]
 
-			A(i * 2 + 0, 0) = -1 * src_pts[i][0];
-			A(i * 2 + 0, 1) = -1 * src_pts[i][1];
-			A(i * 2 + 0, 2) = -1;
-			A(i * 2 + 0, 6) = tgt_pts[i][0] * src_pts[i][0];
-			A(i * 2 + 0, 7) = tgt_pts[i][0] * src_pts[i][1];
-			A(i * 2 + 0, 8) = tgt_pts[i][0];
+		A(i * 2 + 0, 0) = -1 * src_n[i][0];
+		A(i * 2 + 0, 1) = -1 * src_n[i][1];
+		A(i * 2 + 0, 2) = -1;
+		A(i * 2 + 0, 6) = tgt_n[i][0] * src_n[i][0];
+		A(i * 2 + 0, 7) = tgt_n[i][0] * src_n[i][1];
+		A(i * 2 + 0, 8) = tgt_n[i][0];
 
-			A(i * 2 + 1, 3) = -1 * src_pts[i][0];
-			A(i * 2 + 1, 4) = -1 * src_pts[i][1];
-			A(i * 2 + 1, 5) = -1;
-			A(i * 2 + 1, 6) = tgt_pts[i][1] * src_pts[i][0];
-			A(i * 2 + 1, 7) = tgt_pts[i][1] * src_pts[i][1];
-			A(i * 2 + 1, 8) = tgt_pts[i][1];
-		}
+		A(i * 2 + 1, 3) = -1 * src_n[i][0];
+		A(i * 2 + 1, 4) = -1 * src_n[i][1];
+		A(i * 2 + 1, 5) = -1;
+		A(i * 2 + 1, 6) = tgt_n[i][1] * src_n[i][0];
+		A(i * 2 + 1, 7) = tgt_n[i][1] * src_n[i][1];
+		A(i * 2 + 1, 8) = tgt_n[i][1];
 	}
 
 	// 3. solve nullspace of A for H;
@@ -125,4 +121,60 @@ void DLT(const std::vector<cv::vec2r> &src_pts, const std::vector<cv::vec2r> &tg
 	H = invTgtS * H * srcS;
 }
 
+/*
+ * Pack homography matrices A and B by the form used for least squares solving.
+ */
+void packHomographyAB(const std::vector<cv::vec2r> &src_pts, const std::vector<cv::vec2r> &tgt_pts, cv::matrixr &A, cv::matrixr &B) {
+
+	ASSERT(src_pts.size() && src_pts.size() == tgt_pts.size());
+
+	// construct matrices
+	A = cv::matrixr::zeros(src_pts.size() * 2, 8);
+	B.create(src_pts.size() * 2, 1);
+
+	// populate matrices with data.
+	for (unsigned i = 0; i < src_pts.size(); i++) {
+
+		auto &src = src_pts[i];
+		auto &tgt = tgt_pts[i];
+
+		B(i * 2, 0) = tgt[0];
+		B(i * 2 + 1, 0) = tgt[1];
+
+		A(i * 2, 0) = src[0];
+		A(i * 2, 1) = src[1];
+		A(i * 2, 2) = 1;
+		A(i * 2 + 1, 3) = src[0];
+		A(i * 2 + 1, 4) = src[1];
+		A(i * 2 + 1, 5) = 1;
+
+		A(i * 2, 6) = -1 * src[0] * tgt[0];
+		A(i * 2, 7) = -1 * src[1] * tgt[0];
+		A(i * 2 + 1, 6) = -1 * src[0] * tgt[1];
+		A(i * 2 + 1, 7) = -1 * src[1] * tgt[1];
+	}
+}
+
+/*
+ * Solve homography using least squares method.
+ */
+void solveLeastSquaresHomography(const std::vector<cv::vec2r> &src_pts, const std::vector<cv::vec2r> &tgt_pts, cv::matrixr &H) {
+
+	cv::matrixr A, B;
+	packHomographyAB(src_pts, tgt_pts, A, B);
+	cv::matrixr _H(8, 1);
+
+	cv::matrixr At = A.transposed();
+	lu_solve(At * A, At * B, _H);
+
+	if (!_H) {
+		throw std::runtime_error("Internal error!~ failure occurred calculating homography.\n");
+	}
+
+	H.create(1, 9);
+	std::copy(_H.begin(), _H.end(), H.begin());
+	H.reshape(3, 3);
+	H(2, 2) = 1;
+
+}
 
