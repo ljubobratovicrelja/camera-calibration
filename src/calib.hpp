55c075f2be0d57a05f99c115ec863ad2c8beaed4
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-15 19:29:15 +0200

    commenting

--- src/calib.hpp
+++ src/calib.hpp
@@ -1,31 +1,3 @@
-// The MIT License (MIT)
-//
-// Copyright (c) 2015 Relja Ljubobratovic, ljubobratovic.relja@gmail.com
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-// Description:
-// Collection of algoritms for calibration data computation.
-//
-// Author:
-// Relja Ljubobratovic, ljubobratovic.relja@gmail.com
-
 
 #ifndef CALIB_HPP_Q5H9DDXY
 #define CALIB_HPP_Q5H9DDXY
@@ -136,6 +108,16 @@ cv::matrixr compute_extrinsics(const cv::matrixr &A, const cv::matrixr &H);
 cv::vec2r compute_distortion(const std::vector<std::vector<cv::vec2r>> &image_pts, const std::vector<std::vector<cv::vec2r>> &image_pts_nrm,
                              const std::vector<std::vector<cv::vec2r>> &image_pts_proj, const cv::matrixr &A);
 
+/*!
+ * @brief Draw the reprojected points, from world to image plane, using calculated camera parameters.
+ *
+ * Draws image points with smaller blue circle, and reprojection with larger red circles.
+ *
+ * @param image_pts image points from calibration pattern
+ * @param image_pts_proj reprojected points calculated using calc_reprojection function
+ * @param im_w image width
+ * @param im_h image height
+ */
 cv::matrix3b draw_reprojection(const std::vector<cv::vec2r> &image_pts, 
 		const std::vector<cv::vec2r > &image_pts_proj, unsigned im_w, unsigned im_h);
 
