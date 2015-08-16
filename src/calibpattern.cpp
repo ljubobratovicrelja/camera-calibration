04412ada449d3252fa75fd3688048626f89ffa01
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-16 12:04:22 +0200

    added fitting based implementation of pattern detection, not finished

--- src/calibpattern.cpp
+++ src/calibpattern.cpp
@@ -5,12 +5,17 @@
 
 #include <kdtree.hpp>
 #include <improc.hpp>
+#include <optimization.hpp>
 
 // debug
 #include <gui.hpp>
 #include <draw.hpp>
 
-// forward declaration ////////////////////////////////////////////////////////////////////////////////////////////////
+#ifdef LINE_DEBUG
+cv::matrix3r draw_image;
+#endif
+
+// forward declaration 
 std::vector<std::vector<unsigned> > find_nns(std::vector<cv::vec2r> &features, unsigned nncount);
 
 std::vector<cv::contouri> detect_lines(std::vector<cv::vec2r> &features, std::vector<std::vector<unsigned> > &nns, real_t angleThresh, real_t magThresh, unsigned rows, unsigned cols);
@@ -21,8 +26,7 @@ cv::contouri detect_line(unsigned startIdx, unsigned queryIdx, std::vector<cv::v
 
 std::vector<cv::vec2r> detect_chessboard(const std::vector<cv::contouri> &contours, unsigned rows, unsigned cols);
 
-// Implementation /////////////////////////////////////////////////////////////////////////////////////////////////////
-
+// Implementation 
 template<typename _Tp>
 bool is_aproximation(const _Tp &query, const _Tp &source, const _Tp &error) {
 	return (fabs(source - query) < error);
@@ -80,13 +84,33 @@ cv::contouri detect_line(unsigned startIdx, unsigned queryIdx, std::vector<cv::v
 	cv::contouri line;
 	line.add_points( { features[startIdx], features[queryIdx] });
 
+
+#ifdef LINE_DEBUG
+	auto im = draw_image.clone();
+
+	cv::vec3r line_color = {255., 255., 0.};
+	cv::vec3r nn_color = {0., 0., 255.};
+	cv::vec3r hit_color = {0., 255., 0.};
+	cv::vec3r break_color = {255., 0., 0.};
+
+	cv::draw_circle(im, features[startIdx], 6, line_color);
+	cv::draw_contour(im, line, line_color);
+#endif
+
 	while (true) {
 
 		auto edgeVec = features[queryIdx] - features[startIdx];
 
 		int bestId = -1;
-		real_t bestAngle = 90.0f;
-		real_t bestMagnitureError = 1.0f;
+		real_t bestAngle = angleThreshold;
+		real_t bestMagnitureError = magThreshold;
+
+#ifdef LINE_DEBUG
+		for (auto nn : nns[queryIdx]) {
+			cv::draw_circle(im, features[nn], 3, nn_color);
+		}
+		cv::draw_circle(im, features[queryIdx], 6, line_color);
+#endif
 
 		for (auto nn : nns[queryIdx]) {
 			
@@ -99,10 +123,21 @@ cv::contouri detect_line(unsigned startIdx, unsigned queryIdx, std::vector<cv::v
 			real_t ang = RAD_TO_DEG(edgeVec.angle(queryVec));
 			real_t mag = 1.0 - ((edgeVec.norm() < queryVec.norm()) ? (edgeVec.norm() / queryVec.norm()) : (queryVec.norm() / edgeVec.norm()));
 
-			if (ang < angleThreshold && mag < magThreshold && ang < bestAngle && mag < bestMagnitureError) {
+			if (ang < angleThreshold && mag < magThreshold &&  ang < bestAngle && mag < bestMagnitureError) {
 				bestId = nn;
 				bestAngle = ang;
 				bestMagnitureError = mag;
+#ifdef LINE_DEBUG
+				cv::draw_circle(im, features[bestId], 3, hit_color);
+#endif
+			} else {
+#ifdef LINE_DEBUG
+				cv::draw_circle(im, features[nn], 6, break_color);
+				cv::imshow("line detect", im);
+				auto c = cv::wait_key();
+				if (c == 'q')
+					break;
+#endif
 			}
 		}
 
@@ -110,17 +145,54 @@ cv::contouri detect_line(unsigned startIdx, unsigned queryIdx, std::vector<cv::v
 			startIdx = queryIdx;
 			queryIdx = bestId;
 			line.add_point(features[bestId]);
+
+#ifdef LINE_DEBUG
+			cv::draw_contour(im, line, line_color);
+			for (auto p : line) {
+				cv::draw_circle(im, p, 5, line_color);
+			}
+#endif
 		} else {
+#ifdef LINE_DEBUG
+			cv::draw_contour(im, line, break_color);
+#endif
 			break;
 		}
+
+#ifdef LINE_DEBUG
+		cv::imshow("line detect", im);
+		cv::wait_key();
+#endif
 	}
 
 	unsigned line_size = line.point_length();
 
 	if (line_size == rows or line_size == cols) {
+#ifdef LINE_DEBUG
+		cv::draw_contour(im, line, hit_color);
+		cv::imshow("line detect", im);
+		cv::wait_key();
+#endif
 		return line;
 	} else {
-		return cv::contouri();
+		unsigned lesser = std::min(rows, cols);
+		unsigned larger = std::max(rows, cols);
+		if (line_size > lesser && line_size < larger) {
+			line = cv::contouri(line.begin(), line.begin() + lesser);
+#ifdef LINE_DEBUG
+			cv::draw_contour(im, line, hit_color);
+			cv::imshow("line detect", im);
+			cv::wait_key();
+#endif
+			return line;
+		} else {
+#ifdef LINE_DEBUG
+			cv::draw_contour(im, line, line_color);
+			cv::imshow("line detect", im);
+			cv::wait_key();
+#endif
+			return cv::contouri();
+		}
 	}
 }
 
@@ -285,7 +357,104 @@ std::vector<cv::vec2r> detect_chessboard(const std::vector<cv::contouri> &contou
 	return chessboard_corners;
 }
 
-std::vector<cv::vec2r> detect_pattern(const cv::matrixr &image, unsigned patternRows, unsigned patternCols, real_t angThresh, real_t magThresh, unsigned nnCount) {
+std::vector<cv::vec3d> model_points, corner_points;
+
+void generate_model_points(unsigned p_rows, unsigned p_cols) {
+
+	// get corner point bounding box
+	cv::vec4i bb = {std::numeric_limits<int>::max(), std::numeric_limits<int>::max(),
+		std::numeric_limits<int>::min(), std::numeric_limits<int>::min()};
+
+	for (auto ptn : corner_points) {
+		if (ptn[0] < bb[0]) bb[0] = ptn[0];
+		if (ptn[0] > bb[2]) bb[2] = ptn[0];
+		if (ptn[1] < bb[1]) bb[1] = ptn[1];
+		if (ptn[1] > bb[3]) bb[3] = ptn[1];
+	}
+
+	int x_start = bb[0];
+	int y_start = bb[1];
+
+	int width = bb[2] - bb[0];
+	int height = bb[3] - bb[1];
+
+	int x_step = width / p_cols;
+	int y_step = height / p_rows;
+
+	model_points.clear();
+	model_points.reserve(p_rows*p_cols);
+
+	// pack model points bordered by the bb
+	for (unsigned i = 0; i < p_rows; ++i) {
+		for (unsigned j = 0; j < p_cols; ++j) {
+			model_points.push_back(cv::vec3r(x_start + x_step*j, y_start + y_step*i, 1.));
+		}
+	}
+}
+
+void calib_fix_fcn(int m, int n, double* x, double* fvec,int *iflag) {
+	if (*iflag == 0)
+		return;
+
+	cv::matrixd p_transform(3, 3, x);
+
+	for (unsigned i = 0; i < model_points.size(); ++i) {
+
+		// transform point
+		auto m_ptn = p_transform * model_points[i]; 
+		m_ptn /= m_ptn[2];
+
+		// find nearest point in image corners
+		double b_dist = m_ptn.distance(corner_points[0]);
+		int b_idx = 0;
+
+		for (unsigned j = 1; j < corner_points.size(); ++j) {
+			auto this_dist = m_ptn.distance(corner_points[j]);
+			if (this_dist < b_dist) {
+				b_dist = this_dist;
+				b_idx = j;
+			}
+		}
+		fvec[i] = b_dist;
+	}
+}
+
+std::vector<cv::vec2r> optmize_calib_pattern(std::vector<cv::vec2r> &image_corner_points, 
+		unsigned p_rows, unsigned p_cols, double ftol = 1e-14) {
+
+	ASSERT(image_corner_points.size() >= p_rows*p_cols);
+
+	int info;
+	std::vector<cv::vec2r> model_opt(p_rows*p_cols);
+
+	corner_points.clear();
+	for (unsigned i = 0; i < image_corner_points.size(); ++i) {
+		corner_points.push_back({image_corner_points[i][0], image_corner_points[i][1], 1.});
+	}
+
+	generate_model_points(p_rows, p_cols);
+
+	int m = p_rows*p_cols;
+	int n = 9; 
+
+	cv::matrixd p_transform = cv::matrixd::eye(3);
+
+	info = cv::lmdif1(calib_fix_fcn, m, n, p_transform.data(), ftol);
+
+	for (unsigned i = 0; i < p_rows*p_cols; ++i) {
+		
+		auto m_ptn = p_transform * model_points[i];
+
+		auto h_div = m_ptn[2] ? 1. / m_ptn[2] : 1.;
+
+		model_opt[i][0] = m_ptn[0] * h_div;
+		model_opt[i][1] = m_ptn[1] * h_div;
+	}
+
+	return model_opt;
+}
+
+std::vector<cv::vec2r> detect_pattern(const cv::matrixr &image, unsigned p_rows, unsigned p_cols, real_t angThresh, real_t magThresh, unsigned nnCount) {
 
 	std::vector<cv::vec2r> pattern;
 	std::vector<std::vector<unsigned> > nns;
@@ -293,14 +462,17 @@ std::vector<cv::vec2r> detect_pattern(const cv::matrixr &image, unsigned pattern
 	std::vector<cv::vec2r> features;
 
 	auto h_c = cv::good_features(image, 7);
-	cv::filter_non_maximum(h_c, 20);
-	features = cv::extract_features(h_c, patternCols*patternRows*1.5);
+	cv::filter_non_maximum(h_c, 15);
+	features = cv::extract_features(h_c, p_cols*p_rows*1.5);
+
+#ifdef LINE_DEBUG
+	draw_image = image;
+#endif
 
 	nns = find_nns(features, nnCount);
-	ctns = detect_lines(features, nns, angThresh, magThresh, patternRows, patternCols);
-	pattern = detect_chessboard(ctns, patternRows, patternCols);
+	ctns = detect_lines(features, nns, angThresh, magThresh, p_rows, p_cols);
+	pattern = detect_chessboard(ctns, p_rows, p_cols);
 
 	return pattern;
 }
-
 
