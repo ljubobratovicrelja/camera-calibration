ed579b03fe562dbd8e71dc87691efdd280132bd3
ljubobratovicrelja <ljubobratovic.relja@gmail.com>
2015-08-17 23:35:05 +0200

    added 6 radial and 2 tang distortion parameters in reprojection and optimization

--- src/optimize.hpp
+++ src/optimize.hpp
@@ -24,12 +24,33 @@ int optimize_extrinsics(const std::vector<cv::vec2r> &image_points, const std::v
  * @param model_points World 3D points from calibration pattern.
  * @param A Intrinsic matrix 3x3.
  * @param K Initial extrinsic matrices.
- * @param k Initial value for lens distortion parameters.
+ * @param k Initial value for radial and tangential distortion parameters. As input can be 2, 4, and 8 dimension vector:
+ *	2 - [k1, k2]
+ *	4 - [k1, k2, p1, p2]
+ *	8 - [k1, k2, k3, k4, k5, k6, p1, p2]
+ * Returned value is always 8 point vector with all parameters. For every non-given parameter zero is set as initial value.
  * @param tol Error tolerance used in Levenberg-Marquard optimization algorithm.
  */ 
 int optimize_distortion(const std::vector<std::vector<cv::vec2r>> &image_points, const std::vector<cv::vec3r> &model_points, 
-		const cv::matrixr &A, const std::vector<cv::matrixr> &K, cv::vec2r &k, double tol = 1e-14);
+		const cv::matrixr &A, const std::vector<cv::matrixr> &K, cv::vectorr&k, double tol = 1e-14);
 
+/*!
+ * @brief Optimize all calibration parameters for one pattern.
+ *
+ * @param image_points Image 2D points from calibration pattern.
+ * @param model_points World 3D points from calibration pattern.
+ * @param A Intrinsic matrix 3x3.
+ * @param K Extrinsic matrix 3x4.
+ * @param k Initial value for radial and tangential distortion parameters. As input can be 2, 4, and 8 dimension vector:
+ *	2 - [k1, k2]
+ *	4 - [k1, k2, p1, p2]
+ *	8 - [k1, k2, k3, k4, k5, k6, p1, p2]
+ * @param fixed_aspect Force fixed aspect ration (alpha = beta) in optimization.
+ * @param no_skew Force zero skew (c) in optimization.
+ * @param tol Error tolerance used in Levenberg-Marquard optimization algorithm.
+ */ 
+int optimize_all(const std::vector<cv::vec2r> &image_points, const std::vector<cv::vec3r> &model_points, 
+		cv::matrixr &A, cv::matrixr &K, cv::vectorr &k, bool fixed_aspect = false, bool no_skew = false, double tol = 1e-14);
 /*!
  * @brief Optimize all calibration parameters.
  *
@@ -37,12 +58,15 @@ int optimize_distortion(const std::vector<std::vector<cv::vec2r>> &image_points,
  * @param model_points World 3D points from calibration pattern.
  * @param A Intrinsic matrix 3x3.
  * @param K Extrinsic matrices.
- * @param k Lens distortion parameters.
+ * @param k Initial value for radial and tangential distortion parameters. As input can be 2, 4, and 8 dimension vector:
+ *	2 - [k1, k2]
+ *	4 - [k1, k2, p1, p2]
+ *	8 - [k1, k2, k3, k4, k5, k6, p1, p2]
  * @param fixed_aspect Force fixed aspect ration (alpha = beta) in optimization.
  * @param no_skew Force zero skew (c) in optimization.
  * @param tol Error tolerance used in Levenberg-Marquard optimization algorithm.
  */ 
 int optimize_all(const std::vector<std::vector<cv::vec2r>> &image_points, const std::vector<cv::vec3r> &model_points, 
-		cv::matrixr &A, std::vector<cv::matrixr> &K, cv::vec2r &k, bool fixed_aspect = false, bool no_skew = false, double tol = 1e-14);
+		cv::matrixr &A, std::vector<cv::matrixr> &K, cv::vectorr &k, bool fixed_aspect = false, bool no_skew = false, double tol = 1e-14);
 
 #endif /* end of include guard: OPTIMIZE_HPP_OBWEIHS0 */
